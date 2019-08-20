1f6a02da4a9602bd6583394dec51158d3e4c37b8
Yarra Dileep Kumar <dileepyarra16@gmail.com>
2019-08-20 12:22:43 +0530

    Add files via upload

---
+++ actions.py
@@ -0,0 +1,44 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/core/actions/#custom-actions/
+
+
+# This is a simple example for a custom action which utters "Hello World!"
+
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+import psycopg2
+conn = psycopg2.connect(database = "exam", user = "postgres", password = "1234", host = "localhost", port = "5432")
+cur = conn.cursor()
+
+class ActionHelloWorld(Action):
+
+     def name(self) -> Text:
+         return "action_hello_world"
+
+     def run(self, dispatcher: CollectingDispatcher,
+             tracker: Tracker,
+             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+         city = tracker.get_slot('city')
+         print(city)
+         sql="SELECT TEMPERATURE FROM WEATHER WHERE city='%s'"%city.upper()
+         cur.execute(sql)
+         print(sql)
+         record = cur.fetchone()
+         print(record,record[0])
+         temp=str(record[0])
+         speech="The temperature in " + city + " is " + temp
+         dispatcher.utter_message(speech)
+
+         return []
+
+
+
+
+
+
+
+
